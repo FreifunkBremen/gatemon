a944981a0a0c834cff7de118cf4be24affb70fc0
mortzu <mr@planetcyborg.de>
2015-11-13 14:56:17 +0100

    Code beautyness is missing

--- check-all-vpn-exits.sh
+++ check-all-vpn-exits.sh
@@ -4,25 +4,38 @@
 PATH=/usr/lib/nagios/plugins:/bin:/usr/bin:/sbin:/usr/sbin
 
 # Name of runfile
-RUNFILE="/run/$(basename $(readlink -f $0))"
+RUN_FILE="/run/$(basename $(readlink -f $0))"
 
 # Script finish job
 function finish {
-  # Remove run file
-  rm -f "$RUNFILE"
+  if [ $? -eq 0 ]; then
+    # Remove run file
+    if [ -n "$RUN_FILE" ]; then
+      rm -f "$RUN_FILE"
+    fi
+  fi
+
+  if [ -n "$TMP_FILE" ]; then
+    rm -f "$TMP_FILE"
+  fi
 }
 
 trap finish EXIT
 
 # Check for run file
-if [ -f "$RUNFILE" ]; then
+if [ -f "$RUN_FILE" ]; then
   echo 'Runfile does exists!' >&2
   exit 1
 else
-  touch "$RUNFILE"
+  touch "$RUN_FILE"
 fi
 
-TARGET_FILE='/var/www/status.json'
+API_URL=''
+API_TOKEN=''
+
+MESHMON_NAME=''
+MESHMON_PROVIDER=''
+
 NETWORK4_BASE='10.196.0.'
 NETWORK6_BASE='2001:bf7:540::'
 NETWORK_DEVICE='ffhb-mesh'
@@ -47,31 +60,44 @@ fi
 # Generate temporary file
 TMP_FILE="$(mktemp)"
 
-echo '{"vpn-servers":[' > "$TMP_FILE"
+function do_check() {
+  echo -n "\"$1\":[{"
+
+  COUNTER=0
+  for HOST in $2 $3; do
+    let COUNTER=COUNTER+1
+
+    if grep -q '\.' <<<"$HOST"; then
+      echo -n '"ipv4":'
+    else
+      echo -n '"ipv6":'
+    fi
+
+    if $4 $HOST >/dev/null; then
+      echo -n '1'
+    else
+      echo -n '0'
+    fi
+
+    if [ $COUNTER -ne 2 ]; then
+      echo -n ','
+    fi
+  done
+
+  echo '}]'
+}
+
+echo "{\"uuid\":\"$(hostid)\",\"name\":\"${MESHMON_NAME}\",\"provider\":\"${MESHMON_PROVIDER}\",\"vpn-servers\":[" > "$TMP_FILE"
 
 for GATE in $(seq 1 $VPN_NUMBER); do
   ip route add $IP4_TO_FETCH via ${NETWORK4_BASE}${GATE} dev $NETWORK_DEVICE
   ip -6 route add $IP6_TO_FETCH via ${NETWORK6_BASE}${GATE} dev $NETWORK_DEVICE
 
-  echo -n "{\"name\":\"vpn$(printf '%.2d' ${GATE}).bremen.freifunk.net\"" >> "$TMP_FILE"
+  echo "{\"name\":\"vpn$(printf '%.2d' ${GATE}).bremen.freifunk.net\"," >> "$TMP_FILE"
 
-  echo -n ', "ntp_ipv4":' >> "$TMP_FILE"
+  do_check 'ntp' "${NETWORK4_BASE}${GATE}" "${NETWORK6_BASE}${GATE}" 'check_ntp_time -H' >> "$TMP_FILE"
 
-  if check_ntp_time -H ${NETWORK4_BASE}${GATE} >/dev/null; then
-    echo -n '1' >> "$TMP_FILE"
-  else
-    echo -n '0' >> "$TMP_FILE"
-  fi
-
-  echo -n ', "ntp_ipv6":' >> "$TMP_FILE"
-
-  if check_ntp_time -H ${NETWORK6_BASE}${GATE} >/dev/null; then
-    echo -n '1' >> "$TMP_FILE"
-  else
-    echo -n '0' >> "$TMP_FILE"
-  fi
-
-  echo -n ', "dhcp":' >> "$TMP_FILE"
+  echo -n ', "addresses":[{"ipv4":' >> "$TMP_FILE"
 
   if check_dhcp -s ${NETWORK4_BASE}${GATE} -u -i $NETWORK_DEVICE -t 30 >/dev/null; then
     echo -n '1' >> "$TMP_FILE"
@@ -79,23 +105,11 @@ for GATE in $(seq 1 $VPN_NUMBER); do
     echo -n '0' >> "$TMP_FILE"
   fi
 
-  echo -n ', "dns_ipv4":' >> "$TMP_FILE"
+  echo -n '}],' >> "$TMP_FILE"
 
-  if dig +time=2 +short "$HOST_TO_FETCH" A "@${NETWORK4_BASE}${GATE}" >/dev/null; then
-    echo -n '1' >> "$TMP_FILE"
-  else
-    echo -n '0' >> "$TMP_FILE"
-  fi
+  do_check 'dns' "${NETWORK4_BASE}${GATE}" "${NETWORK6_BASE}${GATE}" "check_dns -H $HOST_TO_FETCH -s" >> "$TMP_FILE"
 
-  echo -n ', "dns_ipv6":' >> "$TMP_FILE"
-
-  if dig +time=2 +short "$HOST_TO_FETCH" A "@${NETWORK6_BASE}${GATE}" >/dev/null; then
-    echo -n '1' >> "$TMP_FILE"
-  else
-    echo -n '0' >> "$TMP_FILE"
-  fi
-
-  echo -n ', "ipv4":' >> "$TMP_FILE"
+  echo -n ', "uplink":[{"ipv4":' >> "$TMP_FILE"
 
   if curl -4 --max-time 5 --silent "http://${HOST_TO_FETCH}/" >/dev/null; then
     echo -n '1' >> "$TMP_FILE"
@@ -111,6 +125,8 @@ for GATE in $(seq 1 $VPN_NUMBER); do
     echo -n '0' >> "$TMP_FILE"
   fi
 
+  echo '}]' >> "$TMP_FILE"
+
   if [ $VPN_NUMBER -eq $GATE ]; then
     echo '}' >> "$TMP_FILE"
   else
@@ -123,6 +139,5 @@ done
 
 echo "], \"lastupdated\": \"$(date --iso-8601=seconds)\"}" >> "$TMP_FILE"
 
-# Move to target
-mv "$TMP_FILE" "$TARGET_FILE"
-chmod 644 "$TARGET_FILE"
+# Push to master
+curl -s -X POST -d @${TMP_FILE} "${API_URL}?token=${API_TOKEN}"
