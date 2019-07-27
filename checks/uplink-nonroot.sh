2bd32bf56867c4cf964727d0189ee0ed47ee0e16
mortzu <mortzu@gmx.de>
2019-07-27 17:32:35 +0200

    Transmit error messages

--- checks/uplink-nonroot.sh
+++ checks/uplink-nonroot.sh
@@ -19,6 +19,15 @@ NETWORK_DEVICE="$1"
 SERVER_IP4="$2"
 SERVER_IP6="$3"
 GATE="$4"
+TMP_FILE="$(mktemp)"
+
+# Delete lockfile after completion
+cleanup() {
+  if [ -n "$TMP_FILE" -a -f "$TMP_FILE" ]; then
+    rm -f "$TMP_FILE"
+  fi
+}
+trap cleanup EXIT
 
 if [[ -z "$NETWORK_DEVICE" ]] || [[ -z "$SERVER_IP4" ]] || [[ -z "$SERVER_IP6" ]] || [[ -z "$GATE" ]]; then
   echo "$0 <device> <ipv4> <ipv6> <gatenum>" >&2
@@ -33,13 +42,16 @@ cat <<EOF
 EOF
 
 # IPv4
+ERROR_MESSAGE=''
 ELAPSED_TIME=0
 STATUS_CODE=0
 
-exec 3>&1 4>&2
-ELAPSED_TIME="$( { time curl -4 --max-time 5 --silent --output /dev/null "http://${HOST_TO_FETCH}/" 1>&3 2>&4; } 2>&1)"
+exec 3>&1 4>"$TMP_FILE"
+ELAPSED_TIME="$( { time curl --ipv4 --max-time 5 --show-error --silent --output /dev/null "http://${HOST_TO_FETCH}/" 1>&3 2>&4; } 2>&1)"
 if [[ "$?" = 0 ]]; then
   STATUS_CODE=1
+else
+  ERROR_MESSAGE="$(head -n1 <"$TMP_FILE")"
 fi
 exec 3>&- 4>&-
 
@@ -47,16 +59,22 @@ cat <<EOF
         ipv4:
           status: ${STATUS_CODE}
           time: ${ELAPSED_TIME}
+          error-message: '${ERROR_MESSAGE}'
 EOF
 
+rm -f "$TMP_FILE"
+
 # IPv6
+ERROR_MESSAGE=''
 ELAPSED_TIME=0
 STATUS_CODE=0
 
-exec 3>&1 4>&2
-ELAPSED_TIME="$( { time curl -6 --max-time 5 --silent --output /dev/null "http://${HOST_TO_FETCH}/" 1>&3 2>&4; } 2>&1)"
+exec 3>&1 4>"$TMP_FILE"
+ELAPSED_TIME="$( { time curl --ipv6 --max-time 5 --show-error --silent --output /dev/null "http://${HOST_TO_FETCH}/" 1>&3 2>&4; } 2>&1)"
 if [[ "$?" = 0 ]]; then
   STATUS_CODE=1
+else
+  ERROR_MESSAGE="$(head -n1 <"$TMP_FILE")"
 fi
 exec 3>&- 4>&-
 
@@ -64,4 +82,5 @@ cat <<EOF
         ipv6:
           status: ${STATUS_CODE}
           time: ${ELAPSED_TIME}
+          error-message: '${ERROR_MESSAGE}'
 EOF
